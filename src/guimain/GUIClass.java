6f3bc501eb6b2dba7131851c777523a8001cdc0e
Shah-Abdul <gshahabdul@gmail.com>
2017-04-11 20:06:02 +0530

    Added file support, reading from file requires work

--- src/guimain/GUIClass.java
+++ src/guimain/GUIClass.java
@@ -2,6 +2,12 @@ package guimain;
 import keygens.RC4Gen;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.awt.*;
 //import java.math.*;
 import java.text.NumberFormat;
@@ -11,7 +17,7 @@ public class GUIClass{
 	JTextField text;
 	JFormattedTextField outLength;
 	JLabel output;
-	
+	String op;
 	public GUIClass()
 	{
 		int height = 768, width = 1024;
@@ -23,6 +29,7 @@ public class GUIClass{
 		//Manufacturing panel(s)
 		JPanel maxPanel = new JPanel();
 		JPanel buttonPanel = new JPanel();
+		JPanel buttonPanel2 = new JPanel();
 		JPanel titlePanel = new JPanel();
 		JPanel textPanel = new JPanel();
 		JPanel outputPanel = new JPanel();
@@ -59,14 +66,23 @@ public class GUIClass{
 		userIP.addActionListener (new UserListener());
 		text.setFont(font);
 		outLength.setFont(font);
+		JButton writeToFile= new JButton("Write to file.");
+		writeToFile.addActionListener(new WriterListener());
+		JButton readFromFile=new JButton("Read stored passwords.");
+		readFromFile.addActionListener(new ReaderListener());
 		
 		//Add stuff to panels
+		
 		titlePanel.add(title);
 		textPanel.add(text);
 		descPanel.add(desc);
 		buttonPanel.add(random);
 		buttonPanel.add(separator);
 		buttonPanel.add(userIP);
+		//buttonPanel.add(separator);
+		buttonPanel2.add(writeToFile);
+		buttonPanel2.add(separator);
+		buttonPanel2.add(readFromFile);
 		outTextPanel.add(outLength);
 		lengthPanel.add(lengthTeller);
 		outputPanel.add(output);
@@ -79,6 +95,7 @@ public class GUIClass{
 		
 		maxPanel.setLayout((new BoxLayout(maxPanel, BoxLayout.Y_AXIS)));
 		buttonPanel.setLayout((new BoxLayout(buttonPanel, BoxLayout.X_AXIS)));
+		buttonPanel2.setLayout((new BoxLayout(buttonPanel2, BoxLayout.X_AXIS)));
 		//lengthPanel.setLayout((new BoxLayout(lengthPanel, BoxLayout.X_AXIS)));
 		maxPanel.add(titlePanel);
 		maxPanel.add(descPanel);
@@ -87,6 +104,7 @@ public class GUIClass{
 		maxPanel.add(outTextPanel);
 		maxPanel.add(outputPanel);
 		maxPanel.add(buttonPanel);
+		maxPanel.add(buttonPanel2);
 		maxPanel.setBounds(0, 0, width, height);
 		//buttonPanel.setBounds(20, 50, height - 20, 25);
 		
@@ -100,9 +118,10 @@ public class GUIClass{
 	
 	public class RandomListener implements ActionListener
 	{
-		public void actionPerformed (ActionEvent e)
+			public void actionPerformed (ActionEvent e)
 		{
-			RC4Gen keygen = new RC4Gen();
+			//checking if random was clicked
+			RC4Gen keygen=new RC4Gen();
 			int i = 0, length = (int)( Math.random() * 8 + 8), num = 0;
 			StringBuilder randStr = new StringBuilder();
             String inStr;
@@ -129,8 +148,10 @@ public class GUIClass{
             }
             keygen.ScheduleKey();
             keygen.GeneratePseudoRandom(true);
+            op=keygen.GetOutput();
             output.setText("OUTPUT: " + keygen.GetOutput());
 		}
+	
 	}
 	
 	public class UserListener implements ActionListener
@@ -154,13 +175,68 @@ public class GUIClass{
 			}
 			keygen.ScheduleKey();
 			keygen.GeneratePseudoRandom(false);
+			op=keygen.GetOutput();
 			output.setText("OUTPUT: " + keygen.GetOutput());
+		}
+	
+	}
+	
+	public class WriterListener implements ActionListener{
+		public void actionPerformed(ActionEvent e)
+		{
+			try
+			{
+				writeToFile();
+			}
+			catch(Exception e1)
+			{
+				e1.printStackTrace();
+			}
 		}
 	}
 	
-	public void writeToFile() throws ClassNotFoundException
+	public class ReaderListener implements ActionListener{
+		public void actionPerformed(ActionEvent e)
+		{
+			try
+			{
+				readFromFile();
+			}
+			catch(Exception e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	public void writeToFile() throws Exception
 	{
-		
+		Path p=Paths.get("GeneratedPasswords.txt");
+		try (BufferedWriter writer = Files.newBufferedWriter(p,StandardOpenOption.CREATE,StandardOpenOption.APPEND)) {
+		    writer.write(op);
+		    writer.newLine();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	// EDit this to your liking. Idk enough of GUI to make it print in a frame, and also it's not prinitng all the stored passwords. But rest assured, they exist in the file.
+	
+	public void readFromFile() throws Exception
+	{
+		Path p=Paths.get("GeneratedPasswords.txt");
+		try (BufferedReader reader = Files.newBufferedReader(p)) {
+			while(reader.readLine()!=null)
+			{
+				System.out.print(reader.readLine()+'\n');
+			}	
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args)
